858053ff9343215643273ac3b16756d261d5e922
kaleee100 <158143861+kaleee100@users.noreply.github.com>
2025-10-01 15:41:39 +0300

    Updated

---
+++ lesson1/Task_2/main.py
@@ -0,0 +1,10 @@
+
+while True:
+    name = input("What is your name?: ")
+
+    if name.lower() == "clark kent":
+        print("You are the Superman!\n")
+        break
+    else:
+        print("You are an ordinary person\n")
+
